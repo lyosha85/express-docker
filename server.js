59d18a905635bd9581d447df5ac2d04746c6410b
Alex <lyosha85@gmail.com>
2016-08-28 16:36:10 +0900

    Add database and perform migrations with
    docker-compose run --rm migrations
    docker-compose run --rm fixtures

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+var config = require('./config');
+var db = require('knex')(config);
 var express = require("express");
 
 express()
   .get("*", function(req,res){
-    res.send("Hai there");
+    // res.send("Hai there..");
+    db('users').then(function(data){
+      res.send(data);
+    });
   })
   .listen(3000, function(err){
     if(err){
